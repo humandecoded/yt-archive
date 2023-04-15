7654df6d21eb60d8986094184ebaa26c2f93b21c
Tom <tom@humandecoded.io>
2023-04-14 23:04:26 -0400

    logging errors

--- yt-archive.py
+++ yt-archive.py
@@ -2,7 +2,7 @@ import csv
 import subprocess
 import os 
 import argparse
-
+from datetime import datetime
 # parse out arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--channel-list", required=True, help="path to csv list of youtube channels")
@@ -23,40 +23,45 @@ with open(channel_list_path, "r") as f:
     channel_list = list(reader)
 
 
+log_name = datetime.now().strftime("%Y-%m-%d-%H:%M:%S") + ".log"
+log_path = folder_location + log_name
+with open(log_path, "a") as f:
 # loop through each channel url
-for channel in channel_list:
-    
-    url = channel[0]
-    if folder_location == "":
-        save_location = f"{channel[1].strip()}"
-    else:
-        # check for slash at the end of argument
-        if folder_location[-1] != "/":
-            folder_location = folder_location + "/"
-        save_location = f"{folder_location}{channel[1].strip()}"
-    
-    # if directory doesn't exist, create it
-    if not os.path.isdir(save_location):
-        os.makedirs(save_location)
-
-   # check for audio-only flag
-    if channel[-1].strip() == "audio":
-        file_name_format = "%(upload_date)s - %(title)s - audio.%(ext)s"
-        # run yt_dlp 
-        yt_dlp = subprocess.run(["yt-dlp", 
+    for channel in channel_list:
+        
+        url = channel[0]
+        if folder_location == "":
+            save_location = f"{channel[1].strip()}"
+        else:
+            # check for slash at the end of argument
+            if folder_location[-1] != "/":
+                folder_location = folder_location + "/"
+            save_location = f"{folder_location}{channel[1].strip()}"
+        
+        # if directory doesn't exist, create it
+        if not os.path.isdir(save_location):
+            os.makedirs(save_location)
+        
+        # check for audio-only flag
+        if channel[-1].strip() == "audio":
+            file_name_format = "%(upload_date)s - %(title)s - audio.%(ext)s"
+            # run yt_dlp 
+            yt_dlp = subprocess.run(["yt-dlp", 
+                "-P", f"{save_location}", 
+                "--download-archive", f"{archive_location}",
+                "-o", file_name_format,
+                "-f", "ba", "--dateafter", "20230301",
+                f"{url}"], stderr=f)
+        else:
+            file_name_format = "%(upload_date)s - %(title)s.%(ext)s"
+            yt_dlp = subprocess.run(["yt-dlp", 
             "-P", f"{save_location}", 
             "--download-archive", f"{archive_location}",
-            "-o", file_name_format,
-            "-f", "ba",
-            f"{url}"])
-    else:
-        file_name_format = "%(upload_date)s - %(title)s.%(ext)s"
-        yt_dlp = subprocess.run(["yt-dlp", 
-        "-P", f"{save_location}", 
-        "--download-archive", f"{archive_location}",
-        "-o", file_name_format,
-        f"{url}"])
-
+            "-o", file_name_format, "--dateafter", "20230301",
+            f"{url}"], stderr=f)
+    
+if os.path.getsize(log_path) == 0:
+    os.remove(log_path)
 
 # todo:build a new archive file from existing downloads
 # todo:if no folder provided in csv, use channel name
